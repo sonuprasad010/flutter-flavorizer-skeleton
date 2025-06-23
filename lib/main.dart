59f59b193acad0f73bf2da4db78f857337552775
unknown <Sonu Prasad Dhakal>
2025-06-23 20:35:30 +0545

    added seperate icon for staging environment

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-             Text("This is ${FlavourConfig.instance.name} environment"),
+            Text("This is ${FlavourConfig.instance.name} environment"),
           ],
         ),
       ),
